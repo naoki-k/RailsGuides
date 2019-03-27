7c14e3a86d2d55ad3435430afb03f3b917cae99c
n.kakuta <naoki.k.0820.i@gmail.com>
2019-03-27 22:18:09 +0900

    editの作成

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
   def index
     @article = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -21,8 +22,23 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
